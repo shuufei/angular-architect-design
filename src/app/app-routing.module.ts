a1de6e8a039bc9bc13ad748e1817f6bfb8b1b9d4
shuufei <digital.shuufei@gmail.com>
2020-10-18 12:31:55 +0900

    add page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectPageComponent } from './page/project-page/project-page.component';
+import { SchedulePageComponent } from './page/schedule-page/schedule-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'project',
+    component: ProjectPageComponent,
+  },
+  {
+    path: 'schedule',
+    component: SchedulePageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/project',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskCardComponent } from './component/presentation/task-card/task-card.component';
+import { ProjectPageComponent } from './page/project-page/project-page.component';
+import { SchedulePageComponent } from './page/schedule-page/schedule-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TaskCardComponent, ProjectPageComponent, SchedulePageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
